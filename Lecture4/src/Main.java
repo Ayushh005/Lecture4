60c9ef37d9f148a8a92e028499c1da865c452ee1
Ayush <ayushofficial2323@gmail.com>
2024-06-08 13:01:41 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,23 +1,43 @@
+import java.util.Arrays;
+
 class Main{
 
-    public static void profit(int[] arr){
-        int buy = Integer.MAX_VALUE;
-        int profit = 0;
-        int maxProfit = 0;
-        for (int i=0;i< arr.length;i++){
-            if (buy > arr[i]){
-                buy = arr[i];
-            }
-            profit = arr[i] - buy;
-        if (profit > maxProfit) {
-            maxProfit = profit;
+    public static int[][] subSetHelper(int[] arr, int si){
+        if (si == arr.length){
+            int[][] output = new int[1][0];
+            return output;
         }
-        }
-        System.out.println(maxProfit);
-    }
-    public static void main(String[] args) {
-        int[] arr = {44,30,24,32,35,30,40,38,15};
+        int smallAns[][] = subSetHelper(arr, si+1);
+        int ans[][] = new int[2 * smallAns.length][];
 
-        profit(arr);
+        int k = 0;
+        for (int i = 0; i < smallAns.length; i++){
+            ans[k] = new int[smallAns[i].length];
+            for (int j = 0; j < smallAns[i].length; j++){
+                ans[k][j] = smallAns[i][j];
+            }
+            k++;
+        }
+        for (int i = 0; i < smallAns.length; i++){
+            ans[k] = new int[smallAns[i].length + 1];
+            ans[k][0] = arr[si];
+            for (int j = 1; j <= smallAns[i].length; j++){
+                ans[k][j] = smallAns[i][j - 1];
+            }
+            k++;
+        }
+        return ans;
+    }
+
+    public static void printSubsets(int[][] subsets) {
+        for (int[] subset : subsets) {
+            System.out.println(Arrays.toString(subset));
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 20, 15};
+        int[][] subsets = subSetHelper(arr, 0);
+        printSubsets(subsets);
     }
 }
