07d6e852145ca632abb373b5e20aade0cadb980e
Ayush <ayushofficial2323@gmail.com>
2024-06-19 22:47:20 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,28 +1,25 @@
 class Main{
 
-    public static String removeDuplicate(String str){
-        if (str.length() == 0){
-            return str;
+    public static String countAndSay(int n){
+        if (n == 1){
+            return "1";
         }
+        String say = countAndSay(n-1);
+
+        int count = 0;
         StringBuilder ans = new StringBuilder();
 
-        for (int i=0;i<str.length();i++){
-            char ch = str.charAt(i);
-            int count = 1;
-
-            while (i<str.length()-1 && str.charAt(i) == str.charAt(i+1)){
-                count++;
-                i++;
-            }
-            ans.append(ch);
-            if (count>1){
-                ans.append(count);
+        for (int i=0;i<say.length();i++){
+            count++;
+            if (i == say.length()-1 || say.charAt(i)!=say.charAt(i+1)){
+                ans.append(count).append(say.charAt(i));
+                count = 0;
             }
         }
         return ans.toString();
     }
     public static void main(String[] args) {
-        String str = "aabbbcddeeefggh";
-        System.out.println(removeDuplicate(str));
+        int n = 5;
+        System.out.println(countAndSay(n));
     }
 }
