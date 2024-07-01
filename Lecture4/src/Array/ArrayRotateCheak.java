56ae5eced2f45819d8ae5aea0069f888b4f61bf0
Ayush <ayushofficial2323@gmail.com>
2024-07-01 15:24:40 +0530

    initial commit

--- Lecture4/src/Array/ArrayRotateCheak.java
+++ Lecture4/src/Array/ArrayRotateCheak.java
@@ -1,18 +1,18 @@
 package Array;
 
 public class ArrayRotateCheak {
-    public static int Cheak(int [] arr,int d){
-        for(int i=0;i< arr.length;i++){
-            if (arr[i]>arr[i+1])
-                return (i+1);
+    public static int Check(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1])
+                return (i + 1);
         }
-        return 0;
+        return 0; // if no rotation point is found
     }
-// this is wrong code
-    public static void main(String[] args) {
-        int arr[] = {4,6,4,8,3,9,5};
-        int d = 3;
-        Cheak(arr,d);
 
+    public static void main(String[] args) {
+        int arr[] = {4, 6, 4, 8, 3, 9, 5};
+        int d = 3; // This variable is not needed for the Check method
+        int rotationIndex = Check(arr);
+        System.out.println("Rotation index: " + rotationIndex);
     }
 }
