14ca6c5538c720df8458fed47eaec9207481d344
Ayush <ayushofficial2323@gmail.com>
2024-07-01 15:38:45 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,22 +1,23 @@
-class Main{
+class Main {
 
-    public static void Sort(int[] arr){
-        int n = arr.length;
-        int nonZero = 0;
-        for (int i=0;i< arr.length;i++){
-            if (arr[i] != 0){
-                int temp = arr[nonZero];
-                arr[nonZero] = arr[i];
-                arr[i] = temp;
-                nonZero++;
+    public static void Sort(int[] arr) {
+        for (int i=1;i< arr.length;i++){
+            int j = i-1;
+            int temp = arr[i];
+            while (j >= 0 && arr[j] > temp){
+                arr[j+1] = arr[j];
+                j--;
             }
-        }
-        for (int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+            arr[j+1] = temp;
         }
     }
+
     public static void main(String[] args) {
-        int[] arr = {0,3,0,2,0};
+        int[] arr = {4,2,5,3,1,7,6};
         Sort(arr);
+        System.out.print("Sorted Array : ");
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
