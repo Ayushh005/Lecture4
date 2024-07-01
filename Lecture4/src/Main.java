3f7b3fad2baf76c925ff7d2052aca58a83c40fc8
Ayush <ayushofficial2323@gmail.com>
2024-07-01 11:04:53 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,38 +1,41 @@
 class Main{
 
-    public static void Sort(int[] arr){
-        int zero = 0;
-        int two = arr.length-1;
-        for (int i=0;i< arr.length;i++){
-            if (arr[i] == 0){
-                int temp = arr[zero];
-                arr[zero] = arr[i];
-                arr[i] = temp;
-                zero++;
-            }
-            else if (arr[i] == 2){
-                int temp = arr[two];
-                arr[two] = arr[i];
-                arr[i] = temp;
-                two--;
-            }
-            else {
-                i++;
-            }
+    public static void Sum(int[] arr1,int[]arr2,int[] output){
+        int i = arr1.length-1;
+        int j = arr2.length-1;
+        int k = Math.max(arr1.length, arr2.length);
+        int carry = 0;
+
+        while(i>=0 && j>=0){
+            int sum = arr1[i] + arr2[j] + carry;
+            output[k] = sum % 10;
+            carry = sum / 10;
+
+            i--;j--;k--;
+        }
+        while (i>=0){
+            int sum = arr1[i] + carry;
+            output[k] = sum % 10;
+            carry = sum / 10;
+            i--;k--;
+        }
+        while (j>=0){
+            int sum = arr2[j] + carry;
+            output[k] = sum % 10;
+            carry = sum / 10;
+            j--;k--;
+        }
+        output[0] = carry;
+
+        for (int s=0;s< output.length;s++){
+            System.out.print(output[s]+" ");
         }
     }
     public static void main(String[] args) {
-        int[] arr = {0,1,2,0,2,0,1};
-        System.out.println("Given Array :");
-        for (int i=0;i< arr.length;i++){
-            System.out.print(arr[i]+ " ");
-        }
-        System.out.println();
-        Sort(arr);
+        int[] arr1 = {4,6,4,8,3,9,5};
+        int[] arr2 = {6,9,2,6,8,4};
+        int[] output = new int[8];
 
-        System.out.println("Sorted Array : ");
-        for (int i=0;i< arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
+        Sum(arr1,arr2,output);
     }
 }
