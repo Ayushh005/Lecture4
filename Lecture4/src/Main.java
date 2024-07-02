b2f58845b02bb85fc340a9d3e60d4ee0e412bebf
Ayush <ayushofficial2323@gmail.com>
2024-07-02 17:46:04 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,23 +1,41 @@
 class Main {
 
-    public static void Sort(int[] arr) {
-        for (int i=1;i< arr.length;i++){
-            int j = i-1;
-            int temp = arr[i];
-            while (j >= 0 && arr[j] > temp){
-                arr[j+1] = arr[j];
-                j--;
+
+    public static void sort(int[] arr) {
+        int zero = 0;
+        int two = arr.length-1;
+
+        for (int i=0;i< arr.length-1;i++){
+            if (arr[i] == 0){
+                int temp = arr[zero];
+                arr[zero] = arr[i];
+                arr[i] = temp;
+                zero++;
             }
-            arr[j+1] = temp;
+            else if (arr[i] == 2){
+                int temp = arr[two];
+                arr[two] = arr[i];
+                arr[i] = temp;
+                two--;
+            }
+            else {
+                i++;
+            }
         }
     }
-
     public static void main(String[] args) {
-        int[] arr = {4,2,5,3,1,7,6};
-        Sort(arr);
-        System.out.print("Sorted Array : ");
+        int[] arr = {0,1,2,0,2,0,1};
+        System.out.println("Given Array :");
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+        sort(arr);
+
+        System.out.println("Sorted Array : ");
         for (int i=0;i< arr.length;i++){
             System.out.print(arr[i]+" ");
         }
+
     }
 }
--- Lecture4/src/SearchingAndSorting/SelectionSort.java
+++ Lecture4/src/SearchingAndSorting/SelectionSort.java
@@ -3,26 +3,24 @@ package SearchingAndSorting;
 public class SelectionSort {
     public static void Sort(int arr[]){
         for (int i=0;i< arr.length-1;i++){
-            int min = arr[i];
             int minIndex = i;
             for (int j=i+1;j< arr.length;j++){
-                if (min > arr[j]){
-                    min = arr[j];
+                if (arr[j] < arr[minIndex]){
                     minIndex = j;
                 }
             }
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
         }
     }
     public static void main(String[] args) {
         int[] arr = {4,3,5,1,2,6,7};
-
+        System.out.print("Sorted Array : ");
         Sort(arr);
-        System.out.print("Sorted Array :");
-        for (int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
     }
 }
