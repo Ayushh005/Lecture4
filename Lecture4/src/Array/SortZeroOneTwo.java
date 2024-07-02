46d27c7f480589acec4c328f19965456a82911ba
Ayush <ayushofficial2323@gmail.com>
2024-07-02 17:47:24 +0530

    initial commit

--- Lecture4/src/Array/SortZeroOneTwo.java
+++ Lecture4/src/Array/SortZeroOneTwo.java
@@ -39,3 +39,28 @@ public class SortZeroOneTwo {
         }
     }
 }
+
+
+
+//    public static void sort(int[] arr) {
+//        int zero = 0;
+//        int two = arr.length-1;
+//
+//        for (int i=0;i< arr.length-1;i++){
+//            if (arr[i] == 0){
+//                int temp = arr[zero];
+//                arr[zero] = arr[i];
+//                arr[i] = temp;
+//                zero++;
+//            }
+//            else if (arr[i] == 2){
+//                int temp = arr[two];
+//                arr[two] = arr[i];
+//                arr[i] = temp;
+//                two--;
+//            }
+//            else {
+//                i++;
+//            }
+//        }
+//    }
