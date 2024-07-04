e26396182851a255687770e6d27631d2ca8c0934
Ayush <ayushofficial2323@gmail.com>
2024-07-04 23:08:46 +0530

    initial commit

--- Lecture4/src/TwoDimensionArray/SearchInMatrix.java
+++ Lecture4/src/TwoDimensionArray/SearchInMatrix.java
@@ -1,28 +1,34 @@
 package TwoDimensionArray;
 
 public class SearchInMatrix {
-    public static boolean search(int[][] arr,int x){
-        int i = 0;
-        int row = arr.length;
-        int col = arr[0].length;
-        int j = col-1;
 
-        while (i>=0 && i<row && j>=0 && j<col){
-            if (arr[i][j] == x){
+    // Function to search for a value in a 2D matrix
+    public static boolean search(int[][] arr, int x) {
+        int row = arr.length;    // Number of rows
+        int col = arr[0].length; // Number of columns
+        int i = 0;               // Start at the first row
+        int j = col - 1;         // Start at the last column
+
+        // Loop until the indices are within the bounds of the matrix
+        while (i >= 0 && i < row && j >= 0 && j < col) {
+            if (arr[i][j] == x) { // If the current element is the target value
                 return true;
-            }
-            else if(arr[i][j] > x){
-                j--;
-            }
-            else if(arr[i][j] < x){
-                i++;
+            } else if (arr[i][j] > x) { // If the current element is larger than the target value
+                j--; // Move left
+            } else { // If the current element is smaller than the target value
+                i++; // Move down
             }
         }
-        return false;
+        return false; // Target value not found
     }
+
     public static void main(String[] args) {
-        int arr[][] = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        int x = 60;
-        System.out.println(search(arr,x));
+        int[][] arr = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        };
+        int x = 60; // Target value to search for
+        System.out.println(search(arr, x)); // Print the result of the search
     }
 }
