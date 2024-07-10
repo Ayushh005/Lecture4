fafb206c8bed43a94a2c91231e82d44552df886e
Ayush <ayushofficial2323@gmail.com>
2024-07-10 22:58:15 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,24 +1,57 @@
 class Main {
-    public static void removeDuplicates(int[] arr) {
-        if (arr.length <= 2) {
-            for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + " ");
+    public static void rotate(int[][] arr) {
+        int n = arr.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
             }
-            return;
         }
 
-        int j = 2;
-        for (int i = 2; i < arr.length; i++) {
-            if (arr[i] != arr[j - 2]) {
-                arr[j++] = arr[i];
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[i][n - 1 - j];
+                arr[i][n - 1 - j] = temp;
             }
         }
-        for (int i = 0; i < j; i++) {
-            System.out.print(arr[i] + " ");
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        rotate(arr);
+
+        // Print the rotated matrix
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
         }
     }
-    public static void main(String[] args) {
-        int[] arr = {1, 1, 1, 2, 2, 3};
-        removeDuplicates(arr);
-    }
 }
+
+
+
+
+//   for(int i=0;i<n;i++){               // for reversing
+//        int left = 0;
+//        int right = matrix.length-1;
+//
+//        while(left < right){
+//        int temp = matrix[i][left];
+//        matrix[i][left] = matrix[i][right];
+//        matrix[i][right]= temp;
+//        left++;
+//        right--;
+//        }
+//        }
