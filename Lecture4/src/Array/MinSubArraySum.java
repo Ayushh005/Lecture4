86e1357dda557a386df912785efb954dd647aa8f
Ayush <ayushofficial2323@gmail.com>
2024-07-13 14:08:20 +0530

    initial commit

---
+++ Lecture4/src/Array/MinSubArraySum.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class MinSubArraySum {
+    public static int minSubArrayLen(int target,int[] nums){
+        int low = 0;
+        int high = 0;
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        for (high = 0;high<nums.length;high++){
+            sum += nums[high];
+            while (sum >= target){
+                min = Math.min(min,high - low + 1);
+                sum -= nums[low];
+                low++;
+            }
+        }
+
+        return (min == Integer.MAX_VALUE) ? 0 : min;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        int target = 7;
+        int result = minSubArrayLen(target, nums);
+        System.out.println("Minimal length of a subarray: " + result); // Output: 2
+    }
+}
