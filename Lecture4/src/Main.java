56a62513642536cfee7b42b8142883d71b0b63f4
Ayush <ayushofficial2323@gmail.com>
2024-07-15 22:08:43 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,42 +1,23 @@
 class Main {
-    public static void rotate(int[][] arr) {
+    public static void find(int[] arr) {
         int n = arr.length;
-
-        // Step 1: Transpose the matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int temp = arr[i][j];
-                arr[i][j] = arr[j][i];
-                arr[j][i] = temp;
+        int count = 0;
+        int max = 0;
+        for (int i=0;i<n;i++){
+            if (arr[i] == 1){
+                count++;
             }
-        }
-
-        // Step 2: Reverse each row
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n / 2; j++) {
-                int temp = arr[i][j];
-                arr[i][j] = arr[i][n - 1 - j];
-                arr[i][n - 1 - j] = temp;
+            else {
+                count = 0;
             }
+            max = Math.max(count,max);
         }
+        System.out.println(max);
     }
 
     public static void main(String[] args) {
-        int[][] arr = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
-
-        rotate(arr);
-
-        // Print the rotated matrix
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+        int[] arr = {1,1,0,1,0,1,1,1,1,0,1,0,1,1};
+        find(arr);
     }
 }
 
