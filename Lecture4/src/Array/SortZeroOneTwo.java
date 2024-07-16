3500cddd7131f17fa192c5540ef299ea5fa82cb6
Ayush <ayushofficial2323@gmail.com>
2024-07-16 23:15:39 +0530

    initial commit

--- Lecture4/src/Array/SortZeroOneTwo.java
+++ Lecture4/src/Array/SortZeroOneTwo.java
@@ -1,28 +1,27 @@
 package Array;
 
 public class SortZeroOneTwo {
-    public static void sort(int[] arr){
+    public static void sort(int[] arr) {
         int n = arr.length;
         int i = 0;
         int zero = 0;
-        int two = n-1;
-            while (i<=two){
-                if (arr[i]==0){
-                    int temp = arr[zero];
-                    arr[zero] = arr[i];
-                    arr[i] = temp;
-                    i++;
-                    zero++;
-                } else if (arr[i]==2) {
-                    int temp = arr[i];
-                    arr[i] = arr[two];
-                    arr[two] = temp;
-                    two--;
-                }
-                else {
-                    i++;
-                }
+        int two = n - 1;
+        while (i <= two) {
+            if (arr[i] == 0) {
+                int temp = arr[zero];
+                arr[zero] = arr[i];
+                arr[i] = temp;
+                i++;
+                zero++;
+            } else if (arr[i] == 2) {
+                int temp = arr[i];
+                arr[i] = arr[two];
+                arr[two] = temp;
+                two--;
+            } else {
+                i++;
             }
+        }
     }
     public static void main(String[] args) {
         int[] arr = {0,1,2,0,2,0,1};
