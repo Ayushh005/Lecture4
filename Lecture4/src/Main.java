d049bf56d61547372635d1ea77bcb8083f9d733d
Ayush <ayushofficial2323@gmail.com>
2024-07-20 16:28:44 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,20 +1,32 @@
 class Main {
-    public static String reverse(String str) {
-        String[] words = str.split(" ");
-        int s = 0;
-        int e = words.length-1;
-        while (s<e){
-            String temp = words[s];
-            words[s] = words[e];
-            words[e] = temp;
-            s++;e--;
+    public static boolean isPermutation(String a, String b) {
+        if (a.length() != b.length()){
+            return false;
         }
-        return String.join(" ",words);
+        int freq[] = new int[256];
+
+        for (int i=0;i<256;i++){
+            freq[i] = 0;
+        }
+        for (int i=0;i<a.length();i++){
+            freq[i]++;
+        }
+        for (int i=0;i<b.length();i++){
+            freq[i]--;
+        }
+        for (int i=0;i<256;i++){
+            if (freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        String str = "This is Ayush Sarraf";
-        System.out.println(reverse(str));
+        String a = "listen";
+        String b = "silent";
+
+        System.out.println(isPermutation(a,b));
     }
 }
 
