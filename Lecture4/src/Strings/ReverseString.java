e93c509ab1fa6cf5ed721afb9e4106fcc4ed28ac
Ayush <ayushofficial2323@gmail.com>
2024-07-20 15:22:15 +0530

    initial commit

--- Lecture4/src/Strings/ReverseString.java
+++ Lecture4/src/Strings/ReverseString.java
@@ -19,3 +19,24 @@ public class ReverseString {
         reverse(str);
     }
 }
+
+//class Main {
+//    public static String reverse(String str) {
+//        char[] ch = str.toCharArray();
+//        int s = 0;
+//        int e = str.length()-1;
+//        while (s <= e){
+//            char temp = ch[s];
+//            ch[s] = ch[e];
+//            ch[e] = temp;
+//            s++;e--;
+//        }
+//        return new String(ch);
+//    }
+//
+//    public static void main(String[] args) {
+//        String str = "racecare";
+//        System.out.println(reverse(str));
+//    }
+//}
+
