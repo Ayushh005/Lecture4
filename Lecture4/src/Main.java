c828c47c2fa05133ac4d3402a6294c2d1682b980
Ayush <ayushofficial2323@gmail.com>
2024-07-23 22:15:39 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -4,13 +4,15 @@ class Main {
         int arr[] = {3,4,1,5,2,7,6};
 
         for (int i=0;i< arr.length-1;i++){
+            int minIndex = i;
             for (int j=i+1;j< arr.length;j++){
-                if (arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if (arr[j] < arr[minIndex]){
+                    minIndex = j;
                 }
             }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
         for (int i=0;i< arr.length;i++){
             System.out.print(arr[i]+" ");
