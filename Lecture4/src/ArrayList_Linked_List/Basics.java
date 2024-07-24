797ffa808389eea5af53bae05b9b05ee9a8834b0
Ayush <ayushofficial2323@gmail.com>
2024-07-24 23:45:23 +0530

    initial commit

---
+++ Lecture4/src/ArrayList_Linked_List/Basics.java
@@ -0,0 +1,30 @@
+package ArrayList_Linked_List;
+
+import java.util.ArrayList;
+
+public class Basics {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list1 = new ArrayList<>(5);         //  default 10 in size
+        System.out.println(list1.size());
+        list1.add(15);
+        list1.add(20);
+        list1.add(25);
+        list1.add(2,50);    //  elements from 2 are shifted right ward and 50 is put on this place
+
+        System.out.println(list1.size());
+        System.out.println(list1.get(2));  //  used to print
+
+        for (int i=0;i< list1.size();i++){
+            System.out.print(list1.get(i)+" ");
+        }
+        System.out.println();
+
+        list1.remove(1);          //  all elements shifted left ward
+        list1.set(1,21);                //  used to update number
+
+        for (int elem : list1){         //  for each loop
+            System.out.print(elem +" ");
+        }
+    }
+}
---
+++ Lecture4/src/ArrayList_Linked_List/LinkedListUse.java
@@ -0,0 +1,14 @@
+package ArrayList_Linked_List;
+
+import org.w3c.dom.Node;
+
+public class LinkedListUse {
+    public static void main(String[] args) {
+        Node<Integer> node1 = new Node<Integer>(10);
+        System.out.println(node1.data);
+        System.out.println(node1.next);
+        Node<Integer> node2 = new Node<Integer>(20);
+        node1.next = node2;
+
+    }
+}
