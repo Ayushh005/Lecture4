0628c1ebf865c9e62dfdec60907b33c372a4af79
Ayush <ayushofficial2323@gmail.com>
2024-07-25 12:57:33 +0530

    initial commit

--- Lecture3/src/Main.java
+++ Lecture3/src/Main.java
@@ -1,34 +1,15 @@
 class Main{
 
     public static void main(String[] args) {
-        int n = 4;
+        int n = 6;
 
-        for (int i=1;i<=n;i++) {
-
-            for (int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            for (int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            for (int j=1;j<=i-1;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        //  second part
-        for (int i=n-1;i>=1;i--) {
-
-            for (int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            for (int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            for (int j=1;j<=i-1;j++){
-                System.out.print("*");
-            }
-            System.out.println();
+        int a = 0;
+        int b = 1;
+        for (int i=1;i<=n;i++){
+            int c = a + b;
+            a = b;
+            b = c;
+            System.out.print(a+" ");
         }
     }
 }
--- Lecture3/src/OperatorAndForLoop/FibonacciNo.java
+++ Lecture3/src/OperatorAndForLoop/FibonacciNo.java
@@ -7,9 +7,9 @@ public class FibonacciNo {
         System.out.print("Enter n = ");
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int a = 1;
+        int a = 0;
         int b = 1;
-        for (int i=1;i<n;i++) {
+        for (int i=1;i<=n;i++) {
             int c = a + b ;
             a = b;
             b = c;
