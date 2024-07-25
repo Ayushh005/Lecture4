c1c94187b5e6f22effec337fdfff18d5d5d8da46
Ayush <ayushofficial2323@gmail.com>
2024-07-25 12:41:10 +0530

    initial commit

--- Lecture3/src/Pattern2/DiamondPattern.java
+++ Lecture3/src/Pattern2/DiamondPattern.java
@@ -8,42 +8,70 @@ public class DiamondPattern {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 
-        int firsthalf = (n + 1) / 2;
-        int secondhalf = (n / 2);
+        for (int i=1;i<=n;i++) {
 
-        int i = 1;
-        while (i <= firsthalf) {
-            int space = 1;
-            while (space <= firsthalf - i) {
+            for (int j=1;j<=n-i;j++){
                 System.out.print(" ");
-                space++;
             }
-            int j = 1;
-            while (j <= (2 * i) - 1) {
+            for (int j=1;j<=i;j++){
                 System.out.print("*");
-                j++;
+            }
+            for (int j=1;j<=i-1;j++){
+                System.out.print("*");
             }
             System.out.println();
-            i++;
         }
-            //   Second half
+        //  second part
+        for (int i=n-1;i>=1;i--) {
 
-            i = secondhalf;
-
-            while (i >= 1) {
-                int space = 1;
-                while (space <= (secondhalf - i + 1)) {
-                    System.out.print(" ");
-                    space++;
-                }
-                int j = 1;
-                while (j <= (2 * i) - 1) {
-                    System.out.print("*");
-                    j++;
-                }
-                System.out.println();
-                i--;
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
             }
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for (int j=1;j<=i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+//        int firsthalf = (n + 1) / 2;
+//        int secondhalf = (n / 2);
+//
+//        int i = 1;
+//        while (i <= firsthalf) {
+//            int space = 1;
+//            while (space <= firsthalf - i) {
+//                System.out.print(" ");
+//                space++;
+//            }
+//            int j = 1;
+//            while (j <= (2 * i) - 1) {
+//                System.out.print("*");
+//                j++;
+//            }
+//            System.out.println();
+//            i++;
+//        }
+//            //   Second half
+//
+//            i = secondhalf;
+//
+//            while (i >= 1) {
+//                int space = 1;
+//                while (space <= (secondhalf - i + 1)) {
+//                    System.out.print(" ");
+//                    space++;
+//                }
+//                int j = 1;
+//                while (j <= (2 * i) - 1) {
+//                    System.out.print("*");
+//                    j++;
+//                }
+//                System.out.println();
+//                i--;
+//            }
         }
     }
 
