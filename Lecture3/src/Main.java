86e01252a963b14aac5ffd00cae1fef11527b69b
Ayush <ayushofficial2323@gmail.com>
2024-07-25 11:52:19 +0530

    initial commit

--- Lecture3/src/Main.java
+++ Lecture3/src/Main.java
@@ -3,19 +3,18 @@ class Main{
     public static void main(String[] args) {
         int n = 4;
 
-        int i=1;
-        while (i<=n){
-            int j=1;
-            while (j<=n-i){
+        for (int i=1;i<=n;i++){
+
+            for (int j=1;j<=n-i;j++){
                 System.out.print(" ");
-                j++;
             }
-            while (j<=n){
+            for (int j=1;j<=i;j++){
                 System.out.print("*");
-                j++;
+            }
+            for (int j=1;j<=i-1;j++){
+                System.out.print("*");
             }
             System.out.println();
-            i++;
         }
     }
 }
