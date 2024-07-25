58514a6fe99215349fa124bfd4c695c12a3439a6
Ayush <ayushofficial2323@gmail.com>
2024-07-25 12:05:39 +0530

    initial commit

--- Lecture3/src/Main.java
+++ Lecture3/src/Main.java
@@ -3,15 +3,29 @@ class Main{
     public static void main(String[] args) {
         int n = 4;
 
-        for (int i=1;i<=n;i++){
+        for (int i=1;i<=n;i++) {
 
-            for (int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            for (int j=1;j<=i;j++){
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            for (int j=1;j<=i-1;j++){
+            for (int j = 1; j <= 2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //  second part
+        for (int i=n-1;i>=1;i--) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
