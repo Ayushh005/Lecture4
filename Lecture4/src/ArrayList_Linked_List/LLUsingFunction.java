65883f519074ecaef6aedb7f49c2a908326a23c3
Ayush <ayushofficial2323@gmail.com>
2024-07-26 23:15:01 +0530

    initial commit

---
+++ Lecture4/src/ArrayList_Linked_List/LLUsingFunction.java
@@ -0,0 +1,55 @@
+package ArrayList_Linked_List;
+
+import java.util.Scanner;
+
+public class LLUsingFunction {
+    public static Node<Integer> takeInput(){
+        Node<Integer> head = null, tail = null;
+        Scanner s = new Scanner(System.in);
+        int data = s.nextInt();
+
+        while (data != -1){
+            Node<Integer> newNode = new Node<Integer>(data);
+            if (head == null){
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+//                Node<Integer> temp = head;
+//                while (temp.next != null){
+//                    temp = temp.next;
+//                }
+//                temp.next = newNode;
+                tail.next = newNode;                   //  saves much time complexity with maintain tail node
+                tail = newNode;  //  tail = tial.next
+            }
+            data = s.nextInt();
+        }
+        return head;
+    }
+    public static void print(Node<Integer> head){
+        Node<Integer> temp = head;                     //  store head to temp for further traversing
+        while (head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+        head = temp;
+        while (head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+    public static void main(String[] args) {
+//        Node<Integer> node1 = new Node<>(10);
+//        Node<Integer> node2 = new Node<>(20);
+//        Node<Integer> node3 = new Node<>(30);
+//        Node<Integer> node4 = new Node<>(40);
+//        node1.next = node2;
+//        node2.next = node3;
+//        node3.next = node4;
+//        Node<Integer> head = node1;                     //  making first node head
+        Node<Integer> head = takeInput();
+        print(head);
+    }
+}
--- Lecture4/src/ArrayList_Linked_List/LinkedListUse.java
+++ Lecture4/src/ArrayList_Linked_List/LinkedListUse.java
@@ -1,14 +1,25 @@
 package ArrayList_Linked_List;
 
-import org.w3c.dom.Node;
+
 
 public class LinkedListUse {
     public static void main(String[] args) {
-        Node<Integer> node1 = new Node<Integer>(10);
-        System.out.println(node1.data);
-        System.out.println(node1.next);
-        Node<Integer> node2 = new Node<Integer>(20);
-        node1.next = node2;
+        Node<Integer> node1 = new Node<>(10);     //  making a new node
+        Node<Integer> node2 = new Node<>(20);
+        Node<Integer> node3 = new Node<>(30);
+        node1.next = node2;                            //  link to next node
+        node2.next = node3;
+        Node<Integer> head = node1;                    //  making a head node
 
+        System.out.println("Node 1 data: " + node1.data);
+        System.out.println("Node 1 next: " + node1.next);
+
+
+        System.out.println("Node 1 next data: " + node1.next.data);
+
+        while (head != null){                         //  for traversing
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
     }
 }
