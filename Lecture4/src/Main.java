c8fb4df1038644756520cd5aa0aff6bcfb5e4aa1
Ayush <ayushofficial2323@gmail.com>
2024-07-29 23:01:14 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,22 +1,17 @@
 class Main {
+    public static boolean isSorted(int[] arr,int i){
+        for (i=1;i< arr.length;i++){
+            if (arr[i] > arr[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
-        int arr[] = {3,4,1,5,2,7,6};
-
-        for (int i=0;i< arr.length-1;i++){
-            int minIndex = i;
-            for (int j=i+1;j< arr.length;j++){
-                if (arr[j] < arr[minIndex]){
-                    minIndex = j;
-                }
-            }
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
-        for (int i=0;i< arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
+        int[] arr = {1,2,3,4,5,3};
+        int i = 0;
+        System.out.println(isSorted(arr,i));
     }
 }
 
