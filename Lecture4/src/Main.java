b472ae82bbc0899c5ac833fb97e4f3f99a6acee9
Ayush <ayushofficial2323@gmail.com>
2024-08-09 22:17:35 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,17 +1,33 @@
 class Main {
-    public static boolean isSorted(int[] arr,int i){
-        for (i=1;i< arr.length;i++){
-            if (arr[i] > arr[i-1]){
-                return true;
+    public static void sort(int[] arr){
+        int i = 0;
+        int zero = 0;
+        int two = arr.length-1;
+        while (i <= two){
+            if (arr[i] == 0){
+                int temp = arr[i];
+                arr[i] = arr[zero];
+                arr[zero] = temp;
+                i++;
+                zero++;
+            } else if (arr[i] == 2) {
+                int temp = arr[i];
+                arr[i] = arr[two];
+                arr[two] = temp;
+                two--;                   //  dont iterate i++ here because you cheak that swappwd element again
+            }
+            else {
+                i++;
             }
         }
-        return false;
+        for (i =0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,3};
-        int i = 0;
-        System.out.println(isSorted(arr,i));
+        int[] arr = {1,2,0,2,0,1};
+        sort(arr);
     }
 }
 
