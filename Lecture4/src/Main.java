f9e239adb10e0dc62d1a4d3f5439746a339e27c1
Ayush <ayushofficial2323@gmail.com>
2024-08-13 22:59:50 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,25 +1,35 @@
-class Main {
-    public static String compress(String str){
-        StringBuilder ans = new StringBuilder();
+import java.util.Scanner;
 
-        for (int i=0;i<str.length();i++){
-            char ch = str.charAt(i);
-            int count = 1;
-            while (i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
-                count++;
-                i++;
-            }
-            ans.append(ch);
-            if (count > 1){
-                ans.append(count);
+class Main {
+    public static int diagonalSum(int[][] arr){
+        int n = arr.length;
+        int sum = 0;
+
+        for (int i=0;i< arr.length;i++){
+            for (int j=0;j< arr.length;j++){
+                if (i == j){
+                    sum += arr[i][j];
+                }
             }
         }
-        return ans.toString();
+        return sum;
     }
 
     public static void main(String[] args) {
-        String str = "jhhhaaahhg";
-        System.out.println(compress(str));
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter size of Matrix :");
+        System.out.println();
+        int n = s.nextInt();
+        int[][] arr = new int[n][n];
+
+        System.out.println("Enter elements of Matrix :");
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                arr[i][j] = s.nextInt();
+            }
+        }
+        int ans = diagonalSum(arr);
+        System.out.println("Sum of Diagonals : " + ans);
     }
 }
 
