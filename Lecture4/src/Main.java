e5e54fb1ab7061e1bba471f81df9778dc6e3c796
Ayush <ayushofficial2323@gmail.com>
2024-08-27 22:20:20 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -2,17 +2,17 @@ class Main {
     public static void rotate(int[][] arr){
         int n = arr.length;
 
-        for (int i=0;i< arr.length;i++){
+        for (int i=0;i<n;i++){
             for (int j=i+1;j<n;j++){
                 int temp = arr[i][j];
                 arr[i][j] = arr[j][i];
                 arr[j][i] = temp;
             }
         }
+
         for (int i=0;i< arr.length;i++){
             int left = 0;
             int right = arr.length-1;
-
             while (left < right){
                 int temp = arr[i][left];
                 arr[i][left] = arr[i][right];
