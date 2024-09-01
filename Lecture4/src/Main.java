b180c4ca434b80d876c847def850ad42c6554d72
Ayush <ayushofficial2323@gmail.com>
2024-09-01 12:11:23 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,44 +1,14 @@
 class Main {
-    public static void rotate(int[][] arr){
-        int n = arr.length;
-
-        for (int i=0;i<n;i++){
-            for (int j=i+1;j<n;j++){
-                int temp = arr[i][j];
-                arr[i][j] = arr[j][i];
-                arr[j][i] = temp;
-            }
+    public static void inc(int n){
+        if (n == 0){
+            return;
         }
-
-        for (int i=0;i< arr.length;i++){
-            int left = 0;
-            int right = arr.length-1;
-            while (left < right){
-                int temp = arr[i][left];
-                arr[i][left] = arr[i][right];
-                arr[i][right] = temp;
-                left++;
-                right--;
-            }
-        }
+        inc(n-1);
+        System.out.println(n);
     }
-
     public static void main(String[] args) {
-        int[][] arr = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
-
-        rotate(arr);
-
-        // Print the rotated matrix
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+        int n = 10;
+        inc(n);
     }
 }
 
