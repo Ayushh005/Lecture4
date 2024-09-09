9f668f09b339a0a1f9efbbd70c7a56ebfe7121f0
Ayush <ayushofficial2323@gmail.com>
2024-09-09 22:10:04 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,23 +1,23 @@
 class Main {
     public static double power(double a, int n){
-        if (n == 0){
-            return  1;
+        if(n == 0){
+            return 1;
         }
-        double ans = power(a,n/2);
-        if (n < 0){
+        if (n<0){
             n = -n;
             a = 1/a;
         }
+        double ans = power(a,n/2);
         if (n%2 == 0){
             return ans * ans;
         }
-        else {
+        else{
             return ans * ans * a;
         }
     }
     public static void main(String[] args) {
         int a = 2;
-        int n = -4;
+        int n = -2;
         System.out.println(power(a,n));
     }
 }
