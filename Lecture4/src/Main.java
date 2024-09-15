88b478aabb464c71746a5792c8900c4594dcf14e
Ayush <ayushofficial2323@gmail.com>
2024-09-15 15:27:01 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,24 +1,11 @@
 class Main {
-    public static double power(double a, int n){
-        if(n == 0){
-            return 1;
-        }
-        if (n<0){
-            n = -n;
-            a = 1/a;
-        }
-        double ans = power(a,n/2);
-        if (n%2 == 0){
-            return ans * ans;
-        }
-        else{
-            return ans * ans * a;
-        }
+    public static int con(int[] arr,int x){
+
     }
     public static void main(String[] args) {
-        int a = 2;
-        int n = -2;
-        System.out.println(power(a,n));
+        int[] arr = {1,1,6,3,5,5,5,2,2,2,2};
+        int x = 3;
+        con(arr);
     }
 }
 
