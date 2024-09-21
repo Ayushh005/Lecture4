16893bf30519d141c84905d11825bb7a951f6df5
Ayush <ayushofficial2323@gmail.com>
2024-09-21 23:24:14 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,11 +1,25 @@
 class Main {
-    public static int con(int[] arr,int x){
-
+    public static int con(int[] arr){
+        int n = arr.length;
+        int count = 0;
+        int max = 0;
+        for (int i=1;i<n;i++){
+            if (arr[i] == arr[i-1]){
+                count++;
+            } else {
+                max = Math.max(max , count);
+                count = 0;
+            }
+        }
+        if (arr[n-1] == arr[n-2]) {
+            count++;
+        }
+        max = Math.max(max, count);
+        return max;
     }
     public static void main(String[] args) {
         int[] arr = {1,1,6,3,5,5,5,2,2,2,2};
-        int x = 3;
-        con(arr);
+        System.out.println(con(arr));
     }
 }
 
