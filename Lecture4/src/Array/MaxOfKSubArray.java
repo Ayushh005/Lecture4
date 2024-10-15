ac43b09581d4432803e05d5399956f482c0637d5
Ayush <ayushofficial2323@gmail.com>
2024-10-15 22:35:13 +0530

    initial commit

---
+++ Lecture4/src/Array/MaxOfKSubArray.java
@@ -0,0 +1,16 @@
+//package Array;
+//
+//import java.util.Deque;
+//import java.util.*;
+//
+//public class MaxOfKSubArray {
+//    public static void maxOfKSubArr(int[] arr, int k){
+//        Deque<Integer> deque = new Deque<Integer>();
+//
+//
+//    }
+//    public static void main(String[] args) {
+//        int[] arr = {1, 2, 3, 1, 4, 5, 2, 3, 6};
+//        int k = 3;
+//    }
+//}
