0492bf4498ea787cf4cead91d12b52a3b1d8ee7f
Ayush <ayushofficial2323@gmail.com>
2025-01-20 22:10:21 +0530

    initial commit

---
+++ Lecture3/src/BinaryTree/LevelWiseInput.java
@@ -0,0 +1,76 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class LevelWiseInput {
+    public static void preOrder(BinaryTreeNode<Integer> root){
+        if (root == null){
+            return;
+        }
+        preOrder(root.left);
+        System.out.println(root.data + " ");
+        preOrder(root.right);
+    }
+    public static int countNodes(BinaryTreeNode<Integer> root){
+        if (root == null){
+            return 0;
+        }
+        int count = 0;
+        count += countNodes(root.left);
+        count += countNodes(root.right);
+        return count;
+    }
+    public static ArrayList<Integer> BFSprint(BinaryTreeNode<Integer> root){
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<BinaryTreeNode<Integer>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            BinaryTreeNode<Integer> front = queue.poll();
+            list.add(front.data);
+            if (front.left != null) {
+                queue.offer(front.left);
+            }
+            if (front.right != null) {
+                queue.offer(front.right);
+            }
+        }
+        return list;
+    }
+    public static BinaryTreeNode<Integer> takeLevelWiseInput(){
+        Scanner s = new Scanner(System.in);
+        Queue<BinaryTreeNode<Integer>> queue = new LinkedList<>();
+        int rootNode = s.nextInt();
+        if (rootNode == -1){
+            return null;
+        }
+        BinaryTreeNode<Integer> root = queue.offer(root);
+
+        while (!queue.isEmpty()){
+            BinaryTreeNode<Integer> front;
+            front = queue.poll();
+            System.out.println("Enter left child of "+ front.data);
+            int leftChild = s.nextInt();
+            if (leftChild != -1){
+                BinaryTreeNode<Integer> child = new BinaryTreeNode<>(leftChild);
+                queue.offer(child);
+                front.left = child;
+            }
+            System.out.println("Enter right child of "+ front.data);
+            int rightChild = s.nextInt();
+            if (rightChild != -1){
+                BinaryTreeNode<Integer> child = new BinaryTreeNode<>(rightChild);
+                queue.offer(child);
+                front.right = child;
+            }
+        }
+        return root;
+    }
+    public static void main(String[] args) {
+
+        takeLevelWiseInput();
+
+    }
+}
